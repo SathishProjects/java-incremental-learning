67e2ce49e65ff40178c1ba1fb583679a7170ac64
Sathish kumar <rsathishkumar4@gmail.com>
2021-12-23 22:03:16 +0530

    General (Exception e) cant be first catch block

--- src/javademo/Main.java
+++ src/javademo/Main.java
@@ -2,6 +2,12 @@ package javademo;
 
 public class Main {
 	public static void main(String args[]) {
-		System.out.println("Hello world !...");
+		try {
+			int y = 10 / 0;
+		} catch (ArithmeticException ae) {
+			System.out.println("AE - {}" + ae.getMessage());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
